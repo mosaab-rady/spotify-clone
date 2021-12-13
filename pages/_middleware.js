8728058a2de6278d9de1622b6e17fee0eab49b72
mosaab-rady <mosp.rady@gmail.com>
2021-12-13 19:25:36 +0200

    added token

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,7 +6,7 @@ export async function middleware(req) {
 
   const { pathname } = req.nextUrl;
 
-  if (pathname.includes('/api/auth') || token) {
+  if (token || pathname.includes('/api/auth')) {
     return NextResponse.next();
   }
 
