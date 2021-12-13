3446370aa0be90625b4f70d9406d1085d1b062ae
mosaab-rady <mosp.rady@gmail.com>
2021-12-13 19:47:51 +0200

    added token

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,19 +3,18 @@ import { NextResponse } from 'next/server';
 
 export async function middleware(req) {
   const secret = process.env.JWT_SECRET;
-  const token = await getToken({ req, secret });
-
   const { pathname } = req.nextUrl;
+  const token = getToken({ req, secret }).then((token) => {
+    if (token || pathname.includes('/api/auth')) {
+      return NextResponse.next();
+    }
 
-  console.log('secret is' + secret);
-  console.log('token is' + token);
+    // redirect to log in page if there is no token
+    if (!token && pathname !== '/login') {
+      return NextResponse.redirect('/login');
+    }
+  });
 
-  if (token || pathname.includes('/api/auth')) {
-    return NextResponse.next();
-  }
-
-  // redirect to log in page if there is no token
-  if (!token && pathname !== '/login') {
-    return NextResponse.redirect('/login');
-  }
+  console.log('secret is ' + secret);
+  console.log('token is ' + token);
 }
