3d4959d51964d3c7c52f159bdfb2942865ed35b4
mosaab-rady <mosp.rady@gmail.com>
2021-12-14 11:10:29 +0200

    log in page

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,6 @@
 import { getProviders, signIn } from 'next-auth/react';
 
 export default function Login({ providers }) {
-  console.log(Object.values(providers));
   return (
     <div className='flex flex-col items-center justify-center bg-black min-h-screen w-full'>
       <img
@@ -27,6 +26,8 @@ export default function Login({ providers }) {
 
 export async function getServerSideProps() {
   const providers = await getProviders();
+
+  console.log(providers);
 
   return {
     props: {
