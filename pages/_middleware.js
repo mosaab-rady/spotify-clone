e31198ede63636bb0d8a4f80291053281a4ea6d4
mosaab-rady <mosp.rady@gmail.com>
2021-12-14 09:47:23 +0200

    removed middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,13 +3,10 @@ import { NextResponse } from 'next/server';
 
 export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
-
   const { pathname } = req.nextUrl;
-
   if (pathname.includes('/api/auth') || token) {
     return NextResponse.next();
   }
-
   // redirect to log in page if there is no token
   if (!token && pathname !== '/login') {
     return NextResponse.redirect('/login');
